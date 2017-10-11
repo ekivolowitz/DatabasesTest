78ce3feef62c9d57722d2e5feebb26cb60030c1b
Evan Kivolowitz <ekivolowitz@wisc.edu>
2017-10-10 19:30:45 -0500

    WOO

--- queries.sql
+++ queries.sql
@@ -4,15 +4,15 @@ SELECT Name FROM Department;
 
 SELECT b.Name
 FROM Building b, Department d
-WHERE d.DID == 'CompSci' AND b.DID == d.DID;
+WHERE d.DID = 'CompSci' AND b.DID = d.DID;
 
 SELECT s.Name
 FROM Staff s, Chair c
-WHERE s.SID == c.SID;
+WHERE s.SID = c.SID;
 
 SELECT s1.Name AS Student1, s2.Name AS Student2
 FROM Student s1, Student s2
-WHERE s1.Major = s2.Major;
+WHERE s1.Major = s2.Major AND s1.StudentID <> s2.StudentID;
 
 SELECT Name
 FROM Student
@@ -29,14 +29,22 @@ SELECT Name
 FROM Staff s, Professor p
 WHERE p.SID = s.SID;
 
-SELECT COUNT (DISTINCT StudentID)
+SELECT COUNT (DISTINCT StudentID), Major
 FROM Student
 GROUP BY Major;
 
-SELECT SUM(C.Credits)
+SELECT S.Name, SUM(C.Credits)
 FROM Student S, Takes T, CourseDescription C
 WHERE T.StudentID = S.StudentID AND T.CID = C.CID AND S.Name = 'Evan Kivolowitz';
 
-SELECT AVG(C.Credits)
+
+
+SELECT AVG(creditSUM)
+FROM (
+SELECT S.Name, SUM(C.Credits) AS creditSUM
 FROM Student S, Takes T, CourseDescription C
-WHERE T.StudentID = S.StudentID AND T.CID = C.CID AND S.Year = 'Senior';
+WHERE T.StudentID = S.StudentID AND T.CID = C.CID AND S.Year = 'Senior' AND 
+  T.Semester = 'FALL' AND T.Year = '2017' AND S.Major = 'CompSci'
+GROUP BY S.Name
+);
+
